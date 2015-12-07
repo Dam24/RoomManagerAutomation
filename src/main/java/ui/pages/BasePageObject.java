50c5fdb83ab183af9ade72290ee568e1e52b4475
j4z31 <tumael_777@hotmail.com>
2015-12-07 14:48:06 -0400

    Create the BasePageObject file

---
+++ src/main/java/ui/pages/BasePageObject.java
@@ -0,0 +1,30 @@
+package ui.pages;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: jhasmanyquiroz
+ * Date: 12/7/15
+ * Time: 2:43 PM
+ * To change this template use File | Settings | File Templates.
+ */
+import framework.BrowserManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+
+public abstract class BasePageObject {
+    protected WebDriver driver;
+    protected WebDriverWait wait;
+
+    /**
+     * Initializes the web driver, wait and web elements
+     */
+    public BasePageObject() {
+        this.driver = BrowserManager.getInstance().getDriver();
+        this.wait = BrowserManager.getInstance().getWaitDriver();
+        PageFactory.initElements(driver, this);
+    }
+
+    public abstract void waitUntilPageObjectIsLoaded();
+}
