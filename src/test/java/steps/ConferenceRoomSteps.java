64abdd887e24f8b8847abd9ebf5b2e20dfbd5318
DamianVillanueva <penieldvp18@gmail.com>
2015-12-09 19:05:48 -0400

    commit to do pull upstream

--- src/test/java/steps/ConferenceRoomSteps.java
+++ src/test/java/steps/ConferenceRoomSteps.java
@@ -5,6 +5,7 @@ import cucumber.api.java.en.Given;
 import cucumber.api.java.en.When;
 import ui.PageTransporter;
 import ui.pages.ConferenceRoomsPage;
+import ui.pages.ResourceAssociationsPage;
 import ui.pages.RoomInfoPage;
 
 /**
@@ -17,11 +18,13 @@ import ui.pages.RoomInfoPage;
 public class ConferenceRoomSteps {
     private ConferenceRoomsPage conferenceRoomsPage;
     private RoomInfoPage roomInfoPage;
+    private ResourceAssociationsPage resourceAssociationsPage;
 
     @Given("I navigate to Conference Rooms page")
     public void I_navigate_to_Conference_Rooms_page(){
         conferenceRoomsPage= PageTransporter.getInstance().navigateToConferenceRoomPage();
     }
+
     @And("I displayed the \"(.*?)\" Resource in the Conference Room's Table")
     public void I_displayed_the_Resource_in_the_Conference_Rooms_Table(String  resourceName){
         conferenceRoomsPage.clickOnSpecificResource(resourceName);
@@ -30,15 +33,13 @@ public class ConferenceRoomSteps {
     @When("I associate the \"(.*?)\" Resource to the \"(.*?)\" Conference Room with quantity \"(.*?)\"")
     public void I_associate_the_Resource_to_the_Conference_Room_with_quantity(String resourceName, String roomName, int quantity){
         roomInfoPage=conferenceRoomsPage.clickOnSpecificRoom(roomName);
-
-
-    }
-
-    @Given("aaaaa")
-    public void aaaaa(){
-
+        resourceAssociationsPage=roomInfoPage.
 
     }
+
+
+
+
 
 
 
