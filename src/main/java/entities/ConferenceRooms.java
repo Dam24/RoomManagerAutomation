7ef181a318ea41a64ccf855c6555af5c9d775923
DamianVillanueva <penieldvp18@gmail.com>
2015-12-09 19:58:26 -0400

    Create the ConferenceRooms and Location Entities

---
+++ src/main/java/entities/ConferenceRooms.java
@@ -0,0 +1,53 @@
+package entities;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: DamianVillanueva
+ * Date: 12/9/15
+ * Time: 7:09 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class ConferenceRooms {
+    String Name;
+    String DisplayName;
+    String Code;
+    int Capacity;
+    String Location;
+
+    public void setName(String name){
+        this.Name = name;
+    }
+
+    public String getName(){
+        return Name;
+    }
+
+    public void setDisplayName(String displayName){
+        this.DisplayName = displayName;
+    }
+    public String getDisplayName(){
+        return DisplayName;
+    }
+
+    public void setCode(String code){
+        this.Code = code;
+    }
+    public String getCode(){
+        return Code;
+    }
+
+    public void setCapacity(int capacity){
+        this.Capacity = capacity;
+    }
+    public int getCapacity(){
+        return Capacity;
+    }
+
+    public void setLocation(String location){
+        this.Location = location;
+    }
+    public String getLocation(){
+        return Location;
+    }
+
+}
---
+++ src/main/java/entities/Location.java
@@ -0,0 +1,47 @@
+package entities;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: DamianVillanueva
+ * Date: 12/9/15
+ * Time: 7:26 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Location {
+    String Name;
+    String DisplayName;
+    String ParentLocation;
+    String Description;
+
+    public void setName(String name){
+        this.Name=name;
+    }
+
+    public String getName(){
+        return Name;
+    }
+
+    public void setDisplayName(String displayName){
+        this.DisplayName=displayName;
+    }
+
+    public String getDisplayName(){
+        return DisplayName;
+    }
+
+    public void setParentLocation(String parentLocation){
+        this.ParentLocation=parentLocation;
+    }
+
+    public String getParentLocation(){
+        return ParentLocation;
+    }
+
+    public void setDescription(String description){
+        this.Description=description;
+    }
+
+    public String getDescription(){
+        return Description;
+    }
+}
