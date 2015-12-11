b6685a3deefa6b584f4a0bb93e7d3f039ba7e68d
j4z31 <tumael_777@hotmail.com>
2015-12-11 12:49:45 -0400

    Add the method search resource by criteria

--- src/main/java/framework/DBQuery.java
+++ src/main/java/framework/DBQuery.java
@@ -1,8 +1,14 @@
 package framework;
 
+import com.mongodb.BasicDBObject;
 import com.mongodb.client.FindIterable;
+import entities.Resource;
 import org.bson.Document;
 import com.mongodb.Block;
+
+import java.util.ArrayList;
+
+import static com.mongodb.client.model.Filters.regex;
 
 /**
  * Created with IntelliJ IDEA.
@@ -27,7 +33,7 @@ public class DBQuery {
 
     public String searchByValue(String collection, final String key, String value) {
         final String [] res = new String[1];
-
+        Object objectRes = null;
         try {
             FindIterable<Document> iterable = DBManager.getInstance().getCollection(collection)
                     .find(new Document(key, value ));
@@ -50,6 +56,36 @@ public class DBQuery {
 
     public void closeMongoDB() {
         DBManager.getInstance().closeMongoDB();
+    }
+
+    public ArrayList<Resource> getResourcesBySearchCriteria(String searchCriteria) {
+        final ArrayList<Resource> resources = new ArrayList<Resource>();
+        try {
+            FindIterable<Document> iterable =
+                    DBManager
+                            .getInstance()
+                            .getCollection("resourcemodels")
+                            .find(
+                                    regex("name", searchCriteria)
+                            );
+
+            iterable.forEach(new Block<Document>() {
+                @Override
+                public void apply(final Document document) {
+                    Resource resource = new Resource();
+                    resource.setName((String)document.get("name"));
+                    resource.setDisplayName((String) document.get("customName"));
+                    resource.setIcon((String) document.get("fontIcon"));
+                    resource.setDescription((String) document.get("description"));
+                    resources.add(resource);
+                }
+            });
+        } catch (Exception e) {
+            System.err.println("ERROR: "+e.getClass().getName() + ": " + e.getMessage());
+        } finally {
+            DBManager.getInstance().closeMongoDB();
+        }
+        return resources;
     }
 
     private void initialize() {
