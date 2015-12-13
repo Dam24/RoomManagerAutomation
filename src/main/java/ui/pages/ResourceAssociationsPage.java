ec1570f8bfc32ea73c6e40134924a4e210505f95
BrayanRosas <brayan_rosas@hotmail.com>
2015-12-13 19:45:10 -0400

    Implementation fixRefreshIsue method in delete resource by UI and move the method createResource to resourceSteps.java

--- src/main/java/ui/pages/ResourceAssociationsPage.java
+++ src/main/java/ui/pages/ResourceAssociationsPage.java
@@ -10,6 +10,7 @@ import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import ui.BaseMainPageObject;
 import ui.BasePageConferenceRoom;
+import ui.PageTransporter;
 
 /**
  * Created with IntelliJ IDEA.
@@ -79,9 +80,10 @@ public class ResourceAssociationsPage extends BasePageConferenceRoom {
     public ResourcePage deleteButtonConfirm(){
         buttonDelete.click();
         isDisplayed(By.xpath("//div[@class='modal-content']"));
-        mainPage.getSideBarMenu().clickOption(EnumOptions.SERVER.option);
-        wait.until(ExpectedConditions.visibilityOf(emailServer));
-        mainPage.getSideBarMenu().clickOptionResource();
+        PageTransporter.getInstance().fixRefreshIsue();
+//        mainPage.getSideBarMenu().clickOption(EnumOptions.SERVER.option);
+//        wait.until(ExpectedConditions.visibilityOf(emailServer));
+//        mainPage.getSideBarMenu().clickOptionResource();
         return new ResourcePage();
     }
 
