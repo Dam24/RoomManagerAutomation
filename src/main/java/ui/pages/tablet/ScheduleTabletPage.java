8a6cda9e4fa718f6eddc2b16c3d64a46a563301a
j4z31 <tumael_777@hotmail.com>
2015-12-13 21:50:27 -0400

    Add the methods with message displayed and meeting present

--- src/main/java/ui/pages/tablet/ScheduleTabletPage.java
+++ src/main/java/ui/pages/tablet/ScheduleTabletPage.java
@@ -1,5 +1,8 @@
 package ui.pages.tablet;
 
+import entities.Meeting;
+import framework.CredentialsManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
@@ -18,9 +21,6 @@ public class ScheduleTabletPage extends BasePageObject {
     @FindBy(xpath= "//div[@class='header-secondary-bar']")
     private WebElement headerBarSecond;
 
-    /*
-    Web elements to create meeting
-     */
     @FindBy(xpath= "//input[@id='txtOrganizer']")
     private WebElement inputOrganizer;
 
@@ -33,14 +33,28 @@ public class ScheduleTabletPage extends BasePageObject {
     @FindBy(xpath= "//input[@type='time' and @ng-model='editable.to']")
     private WebElement inputTo;
 
-    @FindBy(xpath= "//input[@id='//input[@id='_value']")
+    @FindBy(xpath= "//input[@id='_value']")
     private WebElement inputAttendees;
 
-    @FindBy(xpath= "//input[@textarea='txtBody']")
+    @FindBy(xpath= "//textarea[@id='txtBody']")
     private WebElement textAreaBody;
 
     @FindBy(xpath= "//div[@class='form-bar']/button[@class='clean item item-btn']")
     private WebElement buttonCreateMeeting;
+
+    @FindBy(xpath = "//input[contains(@ng-model, 'dialog.credentials.username')]")
+    private WebElement inputCredentialsUsername;
+
+    @FindBy(xpath = "//input[contains(@ng-model, 'dialog.credentials.password')]")
+    private WebElement inputCredentialsPassword;
+
+    @FindBy(xpath = "//button[contains(@ng-click, 'dialog.ok()')]")
+    private WebElement buttonCredentialsAccept;
+
+    @FindBy(css = "div.ng-binding.ng-scope")
+    private WebElement dialogCreateMeetingMessage;
+
+    private Meeting meeting;
 
     public ScheduleTabletPage() {
         PageFactory.initElements(driver, this);
@@ -51,16 +65,53 @@ public class ScheduleTabletPage extends BasePageObject {
         wait.until(ExpectedConditions.visibilityOf(headerBarSecond));
     }
 
-    public MainTablePage createBasicMeeting(String organizer ,String subject,String from,String to,String attendees,String body){
-      inputOrganizer.sendKeys(organizer);
-      inputSubject.sendKeys(subject);
-      inputFrom.sendKeys(from);
-      inputTo.sendKeys(to);
-      inputAttendees.sendKeys(attendees);
-      textAreaBody.sendKeys(body);
-      buttonCreateMeeting.click();
-      return new MainTablePage();
+    public ScheduleTabletPage createBasicMeeting(Meeting meeting){
+        this.meeting = meeting;
+        inputOrganizer.sendKeys(meeting.getOrganizer());
+        inputSubject.sendKeys(meeting.getTitle());
+        inputFrom.clear();
+        inputFrom.sendKeys(meeting.getFrom());
+        inputTo.clear();
+        inputTo.sendKeys(meeting.getTo());
+        inputAttendees.sendKeys(meeting.getAttendees());
+        textAreaBody.sendKeys(meeting.getBody());
+        buttonCreateMeeting.click();
+        return this;
     }
 
+    public ScheduleTabletPage loginCredentialsUserExchange() {
+        inputCredentialsUsername.clear();
+        inputCredentialsUsername
+                .sendKeys(
+                        CredentialsManager
+                                .getInstance()
+                                .getExchangeUserName()
+                )
+        ;
+        inputCredentialsPassword.clear();
+        inputCredentialsPassword
+                .sendKeys(
+                        CredentialsManager
+                                .getInstance()
+                                .getExchangeUserPassword()
+                )
+        ;
+        buttonCredentialsAccept.click();
+        wait.until(ExpectedConditions.visibilityOf(dialogCreateMeetingMessage));
+        return this;
+    }
 
+    public boolean isSuccessfullyMessageDisplayed(String message) {
+        return dialogCreateMeetingMessage.getText().equalsIgnoreCase(message);
+    }
+
+    public boolean isMeetingPresentScheduleBar() {
+        return isPresent(
+                            By
+                            .xpath(
+                                    "//div[@class='vis-item-content']/span[contains(text(), '"+meeting.getTitle()+"')]"
+                            )
+                        )
+        ;
+    }
 }
